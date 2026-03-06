705efe3e97e087deaf80d769fea075234a8245c4
Jack Tyler <hepavista@gmail.com>
2026-03-06 16:38:14 +0200

    Infrastructure Setup: add checkstyle | configure initial ci

---
+++ src/main/java/com/tutego/lamebookstoreapp/LameBookStoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.tutego.lamebookstoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LameBookStoreAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LameBookStoreAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tutego/lamebookstoreapp/LameBookStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutego.lamebookstoreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LameBookStoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
