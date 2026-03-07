9ed64ecbe6315247045eeba05bba9c5fd2730429
Jack Tyler <hepavista@gmail.com>
2026-03-07 21:02:33 +0200

    Implement: Book Entity | Repo | Service

---
+++ src/main/java/com/tutego/lamebookstoreapp/service/BookService.java
@@ -0,0 +1,11 @@
+package com.tutego.lamebookstoreapp.service;
+
+import com.tutego.lamebookstoreapp.model.Book;
+import java.util.List;
+
+public interface BookService {
+    Book save(Book book);
+
+    List<Book> readAll();
+
+}
---
+++ src/main/java/com/tutego/lamebookstoreapp/service/impl/BookServiceImpl.java
@@ -0,0 +1,28 @@
+package com.tutego.lamebookstoreapp.service.impl;
+
+import com.tutego.lamebookstoreapp.model.Book;
+import com.tutego.lamebookstoreapp.repository.BookRepository;
+import com.tutego.lamebookstoreapp.service.BookService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> readAll() {
+        return bookRepository.findAll();
+    }
+}
